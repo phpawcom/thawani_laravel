277b6ee265fa2d2d8fdd970065c0037fcc10605c
Abdulaziz Al Rashdi <me@alrashdi.co>
2024-03-23 21:14:21 +0400

    The commit message seems to be incorrectly associated with the provided code changes. The correct commit message would be:
        Create layout for Thawani view and update payment result page
    
    Introduced a new layout for Thawani view app.blade.php which will be used for all the Thawani related views. Made improvements in the payment result view page, and also added a new canceled page to differentiate between different payment states.

--- src/views/canceled.blade.php
+++ src/views/canceled.blade.php
@@ -1,7 +1,7 @@
 @if(!empty(config('thawani.views.layout')))
-@include('ThawaniLaravel::layouts.adapter')
+    @include('ThawaniLaravel::layouts.adapter')
 @endif
 @extends(!empty(config('thawani.views.layout'))? config('thawani.views.layout') : 'ThawaniLaravel::layouts.app')
 @section(!empty(config('thawani.views.section'))? config('thawani.views.section') : 'content')
-    Payment has been {{ $isSuccessful? 'successful' : 'failed' }}
+    Payment has been canceled
 @endsection
